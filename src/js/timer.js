2e25b950b24cdd0a9de9c5e80f3b91c9642b537c
Simonenko Michail <Qwertyon1993@gmail.com>
2020-10-19 22:12:22 +0300

    second-commit

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,33 +1,27 @@
 const countdownTimer = datePoint => {
+    
+const targetDate = new Date(datePoint);
 
-const targetDate = new Date(datePoint)
-
-    setInterval(() => {
+setInterval(() => {
     const date = new Date();
+    const time = targetDate.getTime() - date;
+    const pad = value => {
+        return String(value).padStart(2, '0')
+    }
+    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
 
-const time = targetDate.getTime() - date;
-        
-        const pad = value => {
-            return String(value).padStart(2, '0')
-        }
-
-const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-
-const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-
-const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-
-const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-
-const valueDaysRefs = document.querySelector('[data-value="days"]');
-const valueHoursRefs = document.querySelector('[data-value="hours"]');
-const valueMinsRefs = document.querySelector('[data-value="mins"]');
-        const valueSecsRefs = document.querySelector('[data-value="secs"]');
-        
-valueDaysRefs.textContent = days;
-valueHoursRefs.textContent = hours;
-valueMinsRefs.textContent = mins;
-valueSecsRefs.textContent = secs;
+    const valueDaysRefs = document.querySelector('[data-value="days"]');
+    const valueHoursRefs = document.querySelector('[data-value="hours"]');
+    const valueMinsRefs = document.querySelector('[data-value="mins"]');
+    const valueSecsRefs = document.querySelector('[data-value="secs"]');
+    
+    valueDaysRefs.textContent = days;
+    valueHoursRefs.textContent = hours;
+    valueMinsRefs.textContent = mins;
+    valueSecsRefs.textContent = secs;
 }, 1000)
 }
 
